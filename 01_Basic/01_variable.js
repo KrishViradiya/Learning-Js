92821a02ddbf48c2d43b8c493cb099d38a2bfd81
Krish Viradiya <krishviradiya1303@gmail.com>
2023-07-30 10:24:41 +0530

    Added two js files including varialbe and datatype

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,5 @@
+const accountId = 12345;
+let name = "krish"
+age=20
+
+console.table([accountId,name,age]);
---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as a newer version
+
+//alert(3+3) // we are using nodejs, not browser
+// code readability should be high
+
+let name = "Krish";
+let age = 20;
+let loggedIn = true;
+
+// number => 2 to power 53
+// bigint
+// String => ""
+// Boolean => true/false
+// null -> standalone value , a type specially , representation of empty value (inteintionally not undefined but null)
+// undefined -> when value is not defined eg: let name;
+// symbol => unique
+
+// Object
+
+console.log(typeof 10);  // number
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
