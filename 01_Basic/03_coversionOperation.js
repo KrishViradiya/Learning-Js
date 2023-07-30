d556c59f22fafc9ddb35490ded1fd69ecc31eff3
Krish Viradiya <krishviradiya1303@gmail.com>
2023-07-30 14:42:57 +0530

    03_coversionOperation.js updaated

--- 01_Basic/03_coversionOperation.js
+++ 01_Basic/03_coversionOperation.js
@@ -5,3 +5,18 @@ let inValueNumber = Number(score)
 
 console.log(typeof inValueNumber);
 console.log(inValueNumber);
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false=> 0;
+
+let isLoggedIn =1;
+let bool = Boolean(isLoggedIn);
+console.log(bool);  // true
+
+let someNumber = 33;
+
+let StringNumber = String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
